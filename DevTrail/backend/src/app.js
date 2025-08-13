d7f0f6dd5ac8fc0cc12359753d599e0fd0a49f86
RachitGandhi-Mern <Rachitgandhi.py@gmail.com>
2025-08-13 19:22:13 +0530

    Backend url chnaged

--- DevTrail/backend/src/app.js
+++ DevTrail/backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dev-trail-chi.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
