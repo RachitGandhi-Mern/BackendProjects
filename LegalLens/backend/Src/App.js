bc14148bfa383f7f8b14c08baaa754556dddf809
RachitGandhi-Mern <Rachitgandhi.py@gmail.com>
2025-08-20 02:28:20 +0530

    changes origin to lastest vercel url

--- LegalLens/backend/Src/App.js
+++ LegalLens/backend/Src/App.js
@@ -18,8 +18,10 @@ ConnectTODB()
 
 app.use(helmet())
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true
+  origin: "https://legalparser.vercel.app", 
+  credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json())
 app.use(cookieParser())
